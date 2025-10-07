fba2e0529c984729ed9436b2ed35736ca39e152e
moollyy <136740176+moollyy@users.noreply.github.com>
2025-10-07 18:22:11 +0000

    grg

--- Main.java
+++ Main.java
@@ -7,6 +7,61 @@ public class Main
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
 
+    // // prob 1 
+    // double target = 12.345;
+    // System.out.println("Enter a number");
+    // double input = scan.nextDouble();
+
+    // if (input == target)
+    // {
+    //   System.out.println("YES!");
+
+    // }
+    // else
+    // {
+    //   System.out.println("NO");
+    // }
+
+    //prob 2 
+    // int target = 48;
+    // System.out.println("Enter a number");
+    // int input = scan.nextInt(); // takes in the users input, basically the user input 
+
+    // if (input == target)
+    // {
+    //   System.out.println("YES!");
+    // }
+    // else
+    // {
+    //   System.out.println("NO"); 
+    // }
+
+    // prob 3 
+    System.out.println("Enter two integers");
+    int input1 = scan.nextInt();
+    int input2 = scan.nextInt();
+
+    // int twice = Mathpow.(input1, 2);
+
+    int twice = input1 + input1; 
+
+    if (input2 == twice)
+    {
+      System.out.println("YES!");
+    }
+    else
+    {
+      System.out.println("NO"); 
+    }
+
+    // prob 4
+    
+
+    
+  
+
+
+    
 
 
 
